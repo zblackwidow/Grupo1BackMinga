40910a750230121d8903be1d701c1f265ccb93ad
Black <alejandrazacariasrt@gmail.com>
2024-11-29 19:29:33 -0300

    XD

--- router/company.js
+++ router/company.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import createCompany from "../controllers/Company/create.js";
 import updateCompany from "../controllers/Company/update.js";
 import deleteCompany from "../controllers/Company/delete.js";
-import readAllCompany from "../controllers/Company/read.js";
+import { readAllCompany, readCompany } from "../controllers/Company/read.js";
 
 
 const companyRouter = Router();
@@ -11,5 +11,6 @@ companyRouter.post("/create", createCompany);
 companyRouter.put("/update/:id", updateCompany);
 companyRouter.delete("/delete/:id", deleteCompany);
 companyRouter.get("/all", readAllCompany);  
+companyRouter.get("/read/:id", readCompany);
 
 export default companyRouter;
