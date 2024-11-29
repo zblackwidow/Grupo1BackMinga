80a0e38767dfb2620a9868bf6be28040db7f2f8e
Black <alejandrazacariasrt@gmail.com>
2024-11-29 20:29:29 -0300

    endpoint update company

--- router/company.js
+++ router/company.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
-import createCompany from "../controllers/Company/create.js";
-import updateCompany from "../controllers/Company/update.js";
-import deleteCompany from "../controllers/Company/delete.js";
 import { readAllCompany, readCompany } from "../controllers/Company/read.js";
+import createCompany from "../controllers/Company/create.js";
+import deleteCompany from "../controllers/Company/delete.js";
+import { updateActive, updateName, updateDescription, updateWebsite, updatePhoto } from "../controllers/Company/update.js";
 
 
 const companyRouter = Router();
 
 companyRouter.post("/create", createCompany);
-companyRouter.put("/update/:id", updateCompany);
 companyRouter.delete("/delete/:id", deleteCompany);
-companyRouter.get("/all", readAllCompany);  
+companyRouter.get("/all", readAllCompany);
 companyRouter.get("/read/:id", readCompany);
+companyRouter.put("/update/active/:id", updateActive);
+companyRouter.put("/update/name/:id", updateName);
+companyRouter.put("/update/description/:id", updateDescription);
+companyRouter.put("/update/website/:id", updateWebsite);
+companyRouter.put("/update/photo/:id", updatePhoto);
 
 export default companyRouter;
