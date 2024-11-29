237bd1bc3a7f10ecd1631d293a886cb071695720
Black <alejandrazacariasrt@gmail.com>
2024-11-28 22:17:00 -0300

    base de datos funcionando

--- router/index.js
+++ router/index.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-
+import companyRouter from "./company.js";
 
 const indexRouter = Router();
 
 
-
+indexRouter.use("/company", companyRouter);
 
 export default indexRouter;
