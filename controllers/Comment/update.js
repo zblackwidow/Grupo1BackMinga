85a7bf74d7d7fe393ec75bf165c507b3731c9f49
JesusVeraPompa <merkapompa@gmail.com>
2024-11-28 21:13:16 -0500

    1.4 Creado los CRUD de Comment

---
+++ controllers/Comment/update.js
@@ -0,0 +1,30 @@
+import Comment from '../../models/Comment.js'
+
+let update = async (req, res, next) => {
+    try {
+        let result = await Comment.findOneAndUpdate(
+            { _id: req.params.id },
+            req.body,
+            { new: true } 
+        )
+        if (result) {
+            return res.status(200).json({
+                success: true,
+                message: 'Updated Comment with SUCCESS',
+                registers_found: `${result._id}`,
+                data: result,
+            })
+        } else {
+            return res.status(200).json({
+                success: false,
+                message: `The update was not performed`,
+                registers_found: `${result._id}`,
+                data: result,
+            })
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+export { update }
