bbe495020652b5b7ff24e7530fbc0214fed39b2a
Javierdrw <javier.jg359@gmail.com>
2024-11-28 21:00:24 -0500

    creando model de manga y reaction a la espera de subir data de manga

--- models/Manga.js
+++ models/Manga.js
@@ -1,3 +1,20 @@
 import { Schema, model } from "mongoose";
 
 let collection = "mangas";
+let schema = new Schema(
+  {
+    author_id: { type: Schema.Types.ObjectId, ref: "author" },
+    company_id: { type: Schema.Types.ObjectId, ref: "company" },
+    title: { type: String, required: true },
+    cover_photo: { type: URL, required: true },
+    description: { type: String, required: true },
+    category_id: { type: Schema.Types.ObjectId, ref: "category" },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+let Manga = model(collection, schema);
+export default Manga;
