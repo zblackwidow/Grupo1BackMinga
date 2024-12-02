1939c22d608aed3c854ecbb473dab89d8b2bde51
sagd27 <guanchezdsa@gmail.comn>
2024-12-02 18:17:23 -0500

    cargando los joi para author y chapter

--- router/index.js
+++ router/index.js
@@ -2,22 +2,18 @@ import { Router } from "express";
 import companyRouter from "./company.js";
 import userRouter from "./user.js";
 import commentRouter from "./comment.js";
-<<<<<<< HEAD
-import authorRouter from "./author.js";
-
-=======
 import mangaRouter from "./manga.js";
->>>>>>> origin
+import authorRouter from "./author.js";
+import chapterRouter from "./chapter.js";
+
 
 const indexRouter = Router();
 
 indexRouter.use("/company", companyRouter);
 indexRouter.use("/user", userRouter);
 indexRouter.use("/comment", commentRouter);
-<<<<<<< HEAD
-indexRouter.use("/author", authorRouter);
-=======
 indexRouter.use("/manga", mangaRouter);
->>>>>>> origin
+indexRouter.use("/author", authorRouter);
+indexRouter.use("/chapter", chapterRouter);
 
 export default indexRouter;
