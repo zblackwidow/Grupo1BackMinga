803a535f1444d1904b4e725eab15ddab2cf8f49a
Javierdrw <javier.jg359@gmail.com>
2024-12-02 19:18:24 -0500

    actializando Reaction

--- controllers/Reaction/update.js
+++ controllers/Reaction/update.js
@@ -3,12 +3,11 @@ import Reaction from "../../models/Reaction.js";
 const updateReaction = async (req, res, next) => {
     try {
         let { id } = req.body;
-        await Reaction.findByIdAndUpdate(id, req.body);
-        let newReaction = await Reaction.findById(id);
+        let reaction = await Reaction.findByIdAndUpdate(id, req.body,{ new: true });
         res.status(200).json({
             success: true,
             message: "Reaction updated successfully",
-            response: newReaction,
+            response: reaction,
         });
     } catch (error) {
         next(error);
