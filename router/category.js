c3991f842390cef69d94989ca931433dd7e70b3c
Black <alejandrazacariasrt@gmail.com>
2024-12-02 21:29:25 -0300

    changes

--- router/category.js
+++ router/category.js
@@ -10,7 +10,7 @@ router.get("/test", (req, res) => {
 });
 router.post("/", create); // Crear categoría
 router.get("/", read); // Leer todas las categorías
-router.put("/:id", update); // Actualizar categoría por ID
-router.delete("/:id", remove); // Eliminar categoría por ID
+router.put("/:id", update); // Actualizar categoría por ID {cambiar por body}
+router.delete("/:id", remove); // Eliminar categoría por ID {cambiar por body}
 
 export default router;
