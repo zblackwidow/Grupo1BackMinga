3aa045f5d9616a28401a29919d81fc696bf99285
Javierdrw <javier.jg359@gmail.com>
2024-12-04 18:32:44 -0500

    arreglando las referencias de reaction

--- models/Reaction.js
+++ models/Reaction.js
@@ -6,9 +6,9 @@ import "./Company.js";
 let collection = "reactions";
 let schema = new Schema(
     {
-        manga_id: { type: Schema.Types.ObjectId, ref: "Manga" },
-        author_id: { type: Schema.Types.ObjectId, ref: "Author" },
-        company_id: { type: Schema.Types.ObjectId, ref: "Company" },
+        manga_id: { type: Schema.Types.ObjectId, ref: "mangas" },
+        author_id: { type: Schema.Types.ObjectId, ref: "authors" },
+        company_id: { type: Schema.Types.ObjectId, ref: "companies" },
         reaction: { type: Number, required: true },
     },
     {
