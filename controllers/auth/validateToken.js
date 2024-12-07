652b7d36cb11e738a6d52906d338aa843c1d4d8d
Javierdrw <javier.jg359@gmail.com>
2024-12-07 01:40:28 -0500

    agregando y haciendo ajustes de passport tambien se creo una ruta de validateToken

---
+++ controllers/auth/validateToken.js
@@ -0,0 +1,23 @@
+
+
+const validateToken = async (req, res, next) => {
+    try {
+        const userResponse = { ... req.user.toObject() };
+        delete userResponse.password;
+        console.log("pase por aqui");
+        
+
+        return res.status(200).json({
+            success: true,
+            message: "Token valid",
+            user: userResponse,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+}
+
+export default validateToken
